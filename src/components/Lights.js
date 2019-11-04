ea3c6b65feac1766bc861723412a7f52efb5a1aa
Luke Lai <luke@lukeelai.com>
2019-11-04 13:34:34 -0800

    switch to material-ui

--- src/components/Lights.js
+++ src/components/Lights.js
@@ -1,26 +1,17 @@
 import React from "react";
-import Switch from "react-switch";
-
+import Switch from "@material-ui/core/Switch";
 const Lights = props => {
   return (
     <label className="row">
       <span className="col-md-2 text-center">{props.name}</span>
       <Switch
+        className="react-switch"
         checked={props.lights.sampleState[props.number].state.on}
         onChange={() => props.handleChange(props.number)}
-        onColor="#86d3ff"
-        onHandleColor="#2693e6"
-        handleDiameter={30}
-        uncheckedIcon={false}
-        checkedIcon={false}
-        boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
-        activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
-        height={20}
-        width={48}
-        className="react-switch col-md-2 "
-        id="material-switch"
+        value={props.name}
+        color="primary"
+        inputProps={{ "aria-label": "primary checkbox" }}
         disabled={!props.lights.sampleState[props.number].state.reachable}
-        //disabled-switch - if not reachable
       />
     </label>
   );
